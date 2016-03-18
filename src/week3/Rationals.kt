51856595612a0914c18a7d53126f167b4d68758d
Abdelrhman Talat <abdelrhman@arabhosters.com>
2016-03-18 21:39:59 +0200

    it's always better to simpilfy at the beging so we don't fall into stackoverflows

--- src/week3/Rationals.kt
+++ src/week3/Rationals.kt
@@ -1,19 +1,33 @@
 package week3
 
 fun main(args: Array<String>) {
-	val x = Rational(1,2)
+	val x = Rational(1,3)
 	val y = Rational(2,3)
 	val z = x.add(y)
-	print(x.sub(y).sub(z))
+	
 }
 
-class Rational(val numer: Int, val denom: Int){
+class Rational(val numer: Int,val denom: Int) {
+	init {
+		require(denom != 0) { "denominator must be different than zero " }
+	}
+
+	constructor(x: Int): this(x,1)
+
+	private fun gcd(a: Int, b: Int): Int = if (b == 0 ) a else gcd(b, a%b)
+
+
+
 	fun add(that: Rational) = Rational(numer * that.denom + that.numer *denom , denom * that.denom)
-	
+
 	fun sub(that: Rational) = add(that.neg())
-	
+
 	fun neg() = Rational(numer * -1, denom)
-	 
-	override fun toString() = "$numer/$denom"
-	
+
+	fun less(that: Rational) = numer * that.denom < that.numer *denom
+
+	fun max(that: Rational) = if(less(that)) that else this
+
+	override fun toString() = "${numer/gcd(numer,denom)}/${denom/gcd(numer,denom)}"
+
 }
