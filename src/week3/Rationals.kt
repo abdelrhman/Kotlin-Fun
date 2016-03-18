3e1e32914b78d0ee6aa1e56f83f8d329698cf7a1
Abdelrhman Talat <abdelrhman@arabhosters.com>
2016-03-18 22:33:58 +0200

    operator overloading

--- src/week3/Rationals.kt
+++ src/week3/Rationals.kt
@@ -3,8 +3,9 @@ package week3
 fun main(args: Array<String>) {
 	val x = Rational(1,3)
 	val y = Rational(2,3)
-	val z = x.add(y)
-	
+	x + y
+	x == y
+
 }
 
 class Rational(val numer: Int,val denom: Int) {
@@ -18,11 +19,15 @@ class Rational(val numer: Int,val denom: Int) {
 
 
 
-	fun add(that: Rational) = Rational(numer * that.denom + that.numer *denom , denom * that.denom)
+	operator fun plus(that: Rational) = Rational(numer * that.denom + that.numer *denom , denom * that.denom)
 
-	fun sub(that: Rational) = add(that.neg())
 
-	fun neg() = Rational(numer * -1, denom)
+	operator fun minus(that: Rational) = plus(-that)
+
+	fun equals(other: Rational) = numer == other.numer && denom == other.denom
+
+
+	operator fun unaryMinus() = Rational(numer * -1, denom)
 
 	fun less(that: Rational) = numer * that.denom < that.numer *denom
 
